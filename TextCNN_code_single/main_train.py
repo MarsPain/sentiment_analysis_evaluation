0d58c9c47de87c4dbbc51b657280d12ee34db7f2
MarsPain <1434579002@qq.com>
2018-10-12 12:06:16 +0800

    修正学习率没有突变的BUG；将特征向量得到的参数与卷积层得到的参数进行点乘multiply，而不是拼接concat

--- TextCNN_code_single/main_train.py
+++ TextCNN_code_single/main_train.py
@@ -20,15 +20,15 @@ from TextCNN_code_single.model import TextCNN
 
 FLAGS = tf.app.flags.FLAGS
 # 文件路径参数
-tf.app.flags.DEFINE_string("ckpt_dir", "ckpt_2", "checkpoint location for the model")
+tf.app.flags.DEFINE_string("ckpt_dir", "ckpt_4", "checkpoint location for the model")
 tf.app.flags.DEFINE_string("pkl_dir", "pkl", "dir for save pkl file")
 tf.app.flags.DEFINE_string("config_file", "config", "dir for save pkl file")
-tf.app.flags.DEFINE_string("tfidf_path", "./data/tfidf_2.txt", "file for tfidf value dict")
+tf.app.flags.DEFINE_string("tfidf_path", "./data/tfidf.txt", "file for tfidf value dict")
 tf.app.flags.DEFINE_string("train_data_path", "../data/sentiment_analysis_trainingset.csv", "path of traning data.")
 tf.app.flags.DEFINE_string("dev_data_path", "../data/sentiment_analysis_validationset.csv", "path of traning data.")
 tf.app.flags.DEFINE_string("test_data_path", "../data/sentiment_analysis_testa.csv", "path of traning data.")
-tf.app.flags.DEFINE_string("word2vec_model_path", "data/word2vec_word_model.txt", "word2vec's embedding for word")
-# tf.app.flags.DEFINE_string("word2vec_model_path", "data/word2vec_word_model_sg.txt", "word2vec's embedding for word")
+# tf.app.flags.DEFINE_string("word2vec_model_path", "data/word2vec_word_model.txt", "word2vec's embedding for word")
+tf.app.flags.DEFINE_string("word2vec_model_path", "data/word2vec_word_model_sg.txt", "word2vec's embedding for word")
 # tf.app.flags.DEFINE_string("word2vec_model_path", "data/wiki_100.utf8", "word2vec's embedding for word")
 # tf.app.flags.DEFINE_string("word2vec_model_path", "data/word2vec_char_model.txt", "word2vec's embedding for char")
 # tf.app.flags.DEFINE_string("word2vec_model_path", "data/wiki_100.utf8", "word2vec's embedding for char")
@@ -44,7 +44,7 @@ tf.app.flags.DEFINE_integer("num_filters", config.num_filters, "number of filter
 tf.app.flags.DEFINE_integer("max_len", config.max_len, "max sentence length. length should be divide by 3,""which is used by k max pooling.")
 tf.app.flags.DEFINE_integer("top_k", config.top_k, "value of top k for k-max polling")
 tf.app.flags.DEFINE_float("learning_rate", config.learning_rate, "learning rate")  # 0.001
-tf.app.flags.DEFINE_boolean("decay_lr_flag", False, "whether manally decay lr")
+tf.app.flags.DEFINE_boolean("decay_lr_flag", True, "whether manally decay lr")
 tf.app.flags.DEFINE_float("clip_gradients", config.clip_gradients, "clip_gradients")
 tf.app.flags.DEFINE_integer("validate_every", config.validate_every, "Validate every validate_every epochs.")
 tf.app.flags.DEFINE_float("dropout_keep_prob", config.dropout_keep_prob, "dropout keep probability")
@@ -92,7 +92,7 @@ class Main:
         logger.info("start seg train data")
         if not os.path.isdir(FLAGS.pkl_dir):   # 创建存储临时字典数据的目录
             os.makedirs(FLAGS.pkl_dir)
-        string_train_valid = os.path.join(FLAGS.pkl_dir, "string_train_valid_2.pkl")
+        string_train_valid = os.path.join(FLAGS.pkl_dir, "string_train_valid.pkl")
         if os.path.exists(string_train_valid):  # 若word_label_path已存在
             with open(string_train_valid, 'rb') as f:
                 self.string_train, self.string_valid = pickle.load(f)
@@ -120,7 +120,7 @@ class Main:
         logger.info("start get dict")
         if not os.path.isdir(FLAGS.pkl_dir):   # 创建存储临时字典数据的目录
             os.makedirs(FLAGS.pkl_dir)
-        word_label_dict = os.path.join(FLAGS.pkl_dir, "word_label_dict_2.pkl")    # 存储word和label与index之间的双向映射字典
+        word_label_dict = os.path.join(FLAGS.pkl_dir, "word_label_dict.pkl")    # 存储word和label与index之间的双向映射字典
         if os.path.exists(word_label_dict):  # 若word_label_path已存在
             with open(word_label_dict, 'rb') as dict_f:
                 self.word_to_index, self.index_to_word, self.label_to_index, self.index_to_label = pickle.load(dict_f)
@@ -132,7 +132,7 @@ class Main:
 
     def get_data(self):
         logger.info("start get data")
-        train_valid_test = os.path.join(FLAGS.pkl_dir, "train_valid_test_2.pkl")
+        train_valid_test = os.path.join(FLAGS.pkl_dir, "train_valid_test_3.pkl")
         if os.path.exists(train_valid_test):    # 若train_valid_test已被处理和存储
             with open(train_valid_test, 'rb') as data_f:
                 train_data, valid_data, self.label_weight_dict = pickle.load(data_f)
@@ -174,7 +174,7 @@ class Main:
         """
         logger.info("start train")
         column_name_list = self.columns
-        column_name = column_name_list[8]   # 选择评价对象
+        column_name = column_name_list[10]   # 选择评价对象
         logger.info("start %s model train" % column_name)
         tf_config = tf.ConfigProto()
         tf_config.gpu_options.allow_growth = True
@@ -232,7 +232,7 @@ class Main:
         if os.path.exists(model_save_dir):
             print("Restoring Variables from Checkpoint.")
             saver.restore(sess, tf.train.latest_checkpoint(model_save_dir))
-            if FLAGS.decay_lr_flag:
+            if False:
                 for i in range(1):  # decay learning rate if necessary.
                     print(i, "Going to decay learning rate by half.")
                     sess.run(text_cnn.learning_rate_decay_half_op)
