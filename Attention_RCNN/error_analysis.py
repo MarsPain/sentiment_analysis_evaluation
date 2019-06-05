65e7f78d25f9c36ff6c82b5c3916a1389aab7b83
MarsPain <1434579002@qq.com>
2019-06-05 09:29:28 +0800

    基于新增的经验和对高分作品的参考，重新开始研究这个数据集和任务

---
+++ Attention_RCNN/error_analysis.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import re
+import os
+from TextCNN_code_single.utils import load_data_from_csv
+
+
+valid_data_path = "../data/sentiment_analysis_validationset.csv"
+log_predict_error_dir = "error_log"
+
+
+def load_data():
+    validate_data_df = load_data_from_csv(valid_data_path)
+    columns = validate_data_df.columns.tolist()
+    column_name = columns[2]
+    error_path = os.path.join(log_predict_error_dir, column_name + ".csv")
+    error_log_df = load_data_from_csv(error_path)
+    return error_log_df
+
+
+def anayasis(data):
+    # print(data.info())
+    data_group_label = data.groupby(["正确标签"])
+    print(data_group_label.count())
+    data_group_label = data.groupby(["正确标签", "错误标签"])
+    print(data_group_label.count())
+    print(data_group_label.mean())
+    # a = data.loc[data["正确标签"] == 3]
+    # print(a)
+
+
+if __name__ == "__main__":
+    data_df = load_data()
+    anayasis(data_df)
